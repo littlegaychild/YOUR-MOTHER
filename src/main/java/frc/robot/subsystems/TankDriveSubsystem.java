881fdf5e254a862d02f42f386d03acc50d1eef1f
Demetrius Demarcus Bartholomew James III Jr <143535711+littlegaychild@users.noreply.github.com>
2023-10-26 19:19:26 -0500

    made driving worik
    i made diriving work

--- src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
@@ -1,13 +1,17 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.components.TalonComponent;
 
 public class TankDriveSubsystem extends SubsystemBase{
     // Declare left and right side motors here,
     // but don't set them to a value here
+    private TalonComponent leftMotor;
+    private TalonComponent rightMotor;
 
     public TankDriveSubsystem() {
         // In the constructor, now set the motors to a value
+        leftMotor = new TalonComponent(TankDriveConstants.DRIVE_LEFT_MOTOR_ID, TalonMotor.TalonSRX);
     }
 
     // Now create two methods, one for each motor
