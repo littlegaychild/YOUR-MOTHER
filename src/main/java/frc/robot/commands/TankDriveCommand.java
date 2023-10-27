f935dbe6ed30b1249ca7ca7ccf8e3f7751fc5cca
Demetrius Demarcus Bartholomew James III Jr <143535711+littlegaychild@users.noreply.github.com>
2023-10-26 19:38:32 -0500

    made driving work PAR T2
    fix

--- src/main/java/frc/robot/commands/TankDriveCommand.java
+++ src/main/java/frc/robot/commands/TankDriveCommand.java
@@ -18,5 +18,7 @@ public class TankDriveCommand extends CommandBase{
     public void execute() {
         // In this method, call the methods you made in the TankDrive
         // In order to set the percent output of the drive motors
+        m_tankDrive.setLeftMotor(m_controller.getLeftY());
+        m_tankDrive.setRightMotor(m_controller.getRightY());
     }
 }
--- src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.TankDriveConstants;
 import frc.robot.components.TalonComponent;
+import frc.robot.components.TalonComponent.TalonMotor;
 
 public class TankDriveSubsystem extends SubsystemBase{
     // Declare left and right side motors here,
@@ -12,10 +14,27 @@ public class TankDriveSubsystem extends SubsystemBase{
     public TankDriveSubsystem() {
         // In the constructor, now set the motors to a value
         leftMotor = new TalonComponent(TankDriveConstants.DRIVE_LEFT_MOTOR_ID, TalonMotor.TalonSRX);
+        rightMotor = new TalonComponent(TankDriveConstants.DRIVE_RIGHT_MOTOR_ID, TalonMotor.TalonSRX);
     }
 
     // Now create two methods, one for each motor
     // These methods should set the percent
     // Output of the left and right motors respectively
-    
+    /**
+     * @author Gretcyn Miller
+     * @param percent the percenbt output of the ledt motors fom -1 to 1
+     */
+    public void setLeftMotor(double percent) {
+        leftMotor.setOutput(percent);
+    }
+    /**
+     * @author Gretcyn Miller
+     * @param percent the percenbt output of the rith tmorots fom -1 to 1
+     */
+    public void setRightMotor(double percent) {
+        rightMotor.setOutput(percent);
+    }
+
+//EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+//EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 }
